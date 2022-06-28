9bdb75e88f52944fcdbdbdd22ba1f612395548e2
mheeday <66279843+mheeday@users.noreply.github.com>
2022-06-28 16:52:15 +0100

    Added files and deployment required changes

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ var helmet = require('helmet');
 
 
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://mheeday:mheeday@cluster0.kxz3q.mongodb.net/sampleAPI?retryWrites=true&w=majority';
+
+var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
